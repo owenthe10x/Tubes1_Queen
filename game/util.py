24c0d907a66d6a9969f091f1d912794e8eb02085
heisenberg <owentobias21@gmail.com>
2024-02-25 15:50:39 +0700

    Update util.py

--- game/util.py
+++ game/util.py
@@ -15,3 +15,30 @@ def get_direction(current_x, current_y, dest_x, dest_y):
 
 def position_equals(a: Position, b: Position):
     return a.x == b.x and a.y == b.y
+
+
+def get_rank(bots: list, bot_id: int):
+    sorted_bots = sorted(bots, key=lambda b: b.properties.diamonds, reverse=True)
+    for i, bot in enumerate(sorted_bots):
+        if bot.id == bot_id:
+            return i + 1
+
+
+def get_time_left(game_objects: list):
+    for item in game_objects:
+        if item.type == "BotGameObject":
+            return item.properties.milliseconds_left
+
+
+def closest_diamond(pos: Position, diamonds: list):
+    print(
+        "anjay",
+        min(
+            diamonds,
+            key=lambda d: abs(d.position.x - pos.x) + abs(d.position.y - pos.y),
+        ).position,
+    )
+    return min(
+        diamonds,
+        key=lambda d: abs(d.position.x - pos.x) + abs(d.position.y - pos.y),
+    ).position
