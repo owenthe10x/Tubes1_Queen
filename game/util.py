e95fffd586e30a294218beac38569c639cf302a6
heisenberg <owentobias21@gmail.com>
2024-03-03 22:03:55 +0700

    feat: add more functionalities

--- game/util.py
+++ game/util.py
@@ -2,6 +2,17 @@ from .models import Position
 
 
 # Gets Information
+def am_i_in_danger(bot, enemies):
+    for enemy in enemies:
+        if (
+            abs(enemy.position.x - bot.position.x)
+            + abs(enemy.position.y - bot.position.y)
+            <= 2
+        ):
+            return True
+    return False
+
+
 def is_in_between(point1, point2, point3):
     return point1 <= point2 <= point3  # Check if point2 is between point1 and point3
 
@@ -10,6 +21,10 @@ def get_direction(current_x, current_y, dest_x, dest_y, board):
     teleports = get_teleport_info(board.game_objects)
     diamondButton = get_diamond_button(board.game_objects)
     horizontal = True
+    isDestDiaButton = (
+        dest_x == diamondButton.position.x and dest_y == diamondButton.position.y
+    )
+
     # Edge case where the bot, the diamond button, and the destination are in the same line on the edge of the board
     if (
         (
@@ -18,6 +33,7 @@ def get_direction(current_x, current_y, dest_x, dest_y, board):
         )
         and current_y == dest_y
         and current_y == diamondButton.position.y
+        and not isDestDiaButton
     ):
         print("masuk sini")
         if current_y != board.height - 1:
@@ -31,6 +47,7 @@ def get_direction(current_x, current_y, dest_x, dest_y, board):
         )
         and current_x == dest_x
         and current_x == diamondButton.position.x
+        and not isDestDiaButton
     ):
         print("msuk pak eko")
         if current_x != board.width - 1:
@@ -40,9 +57,15 @@ def get_direction(current_x, current_y, dest_x, dest_y, board):
 
     # This will avoid any diamond button on the way to the destionation
     if (
-        is_in_between(current_y, diamondButton.position.y, dest_y)
-        or is_in_between(dest_y, diamondButton.position.y, current_y)
-    ) and (dest_x == diamondButton.position.x or current_y == diamondButton.position.y):
+        (
+            is_in_between(current_y, diamondButton.position.y, dest_y)
+            or is_in_between(dest_y, diamondButton.position.y, current_y)
+        )
+        and (
+            dest_x == diamondButton.position.x or current_y == diamondButton.position.y
+        )
+        and not isDestDiaButton
+    ):
         print("anjay pak eko")
         horizontal = False
 
@@ -88,7 +111,7 @@ def get_direction(current_x, current_y, dest_x, dest_y, board):
 
     if delta_x != 0 and horizontal:
         delta_y = 0
-    else:
+    elif delta_y != 0 and not horizontal:
         delta_x = 0
     return (delta_x, delta_y)
 
@@ -114,6 +137,17 @@ def get_closest_diamond(pos: Position, diamonds: list):
     )
 
 
+def check_if_should_go_for_diamond_button(bot, game_objects, diamonds):
+    return get_time_left(game_objects) < (
+        get_time_to_location(
+            bot.position, get_closest_diamond_position(bot.position, diamonds)
+        )
+        + get_time_to_location(
+            get_closest_diamond_position(bot.position, diamonds), bot.properties.base
+        )
+    )
+
+
 # Gets Location
 def get_diamond_button(game_objects: list):
     for item in game_objects:
@@ -122,10 +156,7 @@ def get_diamond_button(game_objects: list):
 
 
 def get_closest_diamond_position(pos: Position, diamonds: list):
-    return min(
-        diamonds,
-        key=lambda d: abs(d.position.x - pos.x) + abs(d.position.y - pos.y),
-    ).position
+    return get_closest_diamond(pos, diamonds).position
 
 
 def get_closest_blue_diamond_position(pos: Position, diamonds: list):
@@ -162,7 +193,7 @@ def get_rank(bots: list, bot_id: int):
 
 
 def get_time_to_location(current: Position, dest: Position):
-    return 1000 * abs((dest.x - current.x) + (dest.y - current.y))
+    return 1000 * (abs(dest.x - current.x) + abs(dest.y - current.y))
 
 
 # Comparisons
@@ -184,10 +215,30 @@ def killable(my_bot: any, enemy: any, diamond_pos: Position) -> bool:
     )
 
 
-# section paling banyak poin bukan diamond karna red diamond lebih worth it
-def find_diamond_mine(diamonds: list):
-    # return closest diamond to bot which is located in the 5x5 area with the most number of diamonds
-    pass
+# 5x5 section yang paling banyak poin bukan diamond karna red diamond lebih worth it
+def find_diamond_mine(diamonds: list, bot_position: Position):
+    max_tile_diamonds = 0
+    section = []
+    for y in range(11):
+        for x in range(11):
+            tile_diamonds = sum(
+                diamond.properties.points
+                for diamond in diamonds
+                if y <= diamond.position.y < y + 5 and x <= diamond.position.x < x + 5
+            )
+            if tile_diamonds > max_tile_diamonds:
+                section = []
+                max_tile_diamonds = tile_diamonds
+                for diamond in diamonds:
+                    if (
+                        y <= diamond.position.y < y + 5
+                        and x <= diamond.position.x < x + 5
+                    ):
+                        section.append(diamond)
+
+    print("Ini total diamond points", max_tile_diamonds)
+    print("Ini section", section)
+    return get_closest_diamond_position(bot_position, section)
 
 
 # jalan pulang ada teleport yang ngebuat dia muter disitu situ aja
